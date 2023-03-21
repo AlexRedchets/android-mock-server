e5a0d9c2ec7de4145e6bb2fc397519864b157ed7
User <alexey.redchets@gmail.com>
2023-03-21 18:07:16 -0400

    Initial commit

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const auth_controller = require('../controllers/auth_controller');
+const controller = require('../controllers/sample_controller');
 const router  = express.Router();
 
-router.get('/oauth2/v1/authorize', auth_controller.getPassportRes);
-router.post('/v2/authentications', auth_controller.getChallenges);
-router.get('/v1/client-config', auth_controller.getClientConfig);
-router.get('/v1/client-config/dictionary', auth_controller.getDictionary);
-router.post('/v2/authentications/*', auth_controller.postChallenges);
-router.post('/v1/session', auth_controller.postSession);
+router.get('/accounts', controller.getAccounts);
+router.get('/chart', controller.getChart);
+router.get('/orders', controller.getOrders);
+
 router.get('/', (req, res) => {
     res.send('SWM Mock server is running!');
 })
